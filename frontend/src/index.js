d8ca92dc5cb62a73057cec60ec6a85efeb83b603
Isabel Ferrara <ifm95@hotmail.com>
2020-12-02 15:48:44 -0600

    merge 2

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,12 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import Router from './Router';
 import * as serviceWorker from './serviceWorker';
-<<<<<<< HEAD
-import 'antd/dist/antd.css'
-=======
 import { AppCtxProvider } from './hooks/context'
 import 'antd/dist/antd.css';
->>>>>>> 9500e505f2c2ca60c2871babf7bfd86a1fe4c972
 
 ReactDOM.render(
     // <React.StrictMode>
